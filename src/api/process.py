84573abbca82ff3da6d13aa84b48031c4f9e69a3
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-08-06 22:47:32 -0400

    Implement "Answer me" endpoint

--- src/api/process.py
+++ src/api/process.py
@@ -17,7 +17,57 @@ def process_files(document):
         }
         response = Response(json.dumps(data), 200, mimetype='application/json')
     except Exception as e:
-        response = Response(e, 500, mimetype='application/json')
+        data = {
+            "success": False,
+            "error_message": str(e)
+        }
+        response = Response(json.dumps(data), 500, mimetype='application/json')
+    return response
+
+def query(query_text):
+    try:
+
+        doc = fitz.Document()
+        if os.listdir(get_temp_folder()):
+            doc.open(get_temp_folder() + "/" + os.listdir(get_temp_folder())[0])
+        else:
+            raise Exception("No document found")
+
+        embeddings = GPT4AllEmbeddings()
+        n = doc.page_count
+
+        doc_content = ""
+        for i in range(0, n):
+            page_n = doc.load_page(i)
+            page_content = page_n.get_text("text")
+            doc_content += page_content + "\n"
+
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=800,
+            chunk_overlap=200,
+            length_function=len
+        )
+        texts = text_splitter.split_text(doc_content)
+
+        document_search = FAISS.from_texts(texts, embeddings)
+
+        docs = document_search.similarity_search(query_text)
+
+        full_text = [ doc.page_content for doc in docs]
+
+        data = {
+                "success": True,
+                "message": "Query processed successfully",
+                "full_text": full_text
+        }
+        response = Response(json.dumps(data), 200, mimetype='application/json')
+    except Exception as e:
+        data = {
+            "success": False,
+            "error_message": str(e)
+        }
+        response = Response(json.dumps(data), 500, mimetype='application/json')
     return response
 
 def get_temp_folder() -> str:
